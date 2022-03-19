78588571c4bcd93f4f9d8135a8bb1115cbe17ead
George Cirlan <george.x.cirlan@kp.org>
2022-03-19 13:13:21 +0200

    added example higher order function to aid in element creationd

--- main.js
+++ main.js
@@ -1,6 +1,55 @@
 (() => {
   const calendar = document.getElementById('calendar-wrapper');
-  calendar.removeChild(calendar.lastElementChild);
+
+  const header = document.createElement('header');
+
+  function createElement(tagName) {
+    const button = document.createElement(tagName);
+
+    return (props) => {
+      const tag = button.cloneNode(true);
+
+      if (typeof props === 'object') {
+        Object.keys(props).forEach(prop => {
+          tag[prop] = props[prop]
+        })
+      }
+
+      return tag
+    }
+  }
+
+  const button = createElement('button');
+
+  const div = createElement('div')
+
+  calendar.insertAdjacentElement('afterend', div({
+    style: 'background-color: red;padding: 10px',
+    textContent: 'DIV1'
+  }))
+  calendar.insertAdjacentElement('afterend', div({
+    style: 'background-color: black;color: white;padding: 10px',
+    innerText: 'DIV2'
+  }))
+
+
+  const prevMonth = button({
+    name: 'previous-month',
+    type: 'button',
+    innerText: '<<'
+  })
+
+  const prevDay = button({
+    name: 'previous-day',
+    type: 'button',
+    innerText: '<'
+  })
+
+ header.appendChild(prevMonth)
+ header.insertAdjacentElement('beforeend', prevDay)
+
+  calendar.parentNode.querySelector('h3')
+    .insertAdjacentElement('afterend', header)
 
   const section = document.createElement('section');
   const labels = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
